7540f4285bb476671c90f521454874d31b831948
Tyrence <email1@jkbff.com>
2012-02-28 23:08:40 -0700

    update version to 1.1

--- deploy_release.bat
+++ deploy_release.bat
@@ -1,1 +1,1 @@
-mvn -DaltDeploymentRepository=release-repo::default::file:../TyrLibPages/maven2/releases clean deploy
+mvn -DaltDeploymentRepository=release-repo::default::file:../TyrLibPages/maven2/releases clean deploy && pause
