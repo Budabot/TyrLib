60b4bbda2a5aa980da1f4594a1a484234db8738a
Tyrence <email1@jkbff.com>
2012-11-25 05:09:12 -0700

    implemented throttled packet queue for ChannelMessage and PrivateMessageSend packets, with burst

--- src/main/java/com/jkbff/ao/tyrlib/chat/ChatPacketSender.java
+++ src/main/java/com/jkbff/ao/tyrlib/chat/ChatPacketSender.java
@@ -4,18 +4,24 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 
 import com.jkbff.ao.tyrlib.packets.BaseClientPacket;
+import com.jkbff.ao.tyrlib.packets.client.PrivateMessageSend;
+import com.jkbff.ao.tyrlib.packets.server.ChannelMessage;
 
 public class ChatPacketSender extends Thread {
 
 	private final OutputStream outputStream;
 	private final BlockingQueue<BaseClientPacket> packetQueue = new LinkedBlockingQueue<BaseClientPacket>();
+	private final BlockingQueue<BaseClientPacket> throttledPacketQueue = new LinkedBlockingQueue<BaseClientPacket>();
 	private final AOSocket aoBot;
 	private final Logger log = Logger.getLogger(this.getClass());
+	private long queueTime = 0;
+	
+	public static final long TIME_BETWEEN_PACKETS = 2000;
+	public static final long BURST_PACKETS = 3;
 	
 	public ChatPacketSender(OutputStream outputStream, AOSocket aoBot) {
 		this.outputStream = outputStream;
@@ -26,10 +32,17 @@ public class ChatPacketSender extends Thread {
 	public void run() {
         while (!aoBot.shouldStop) {
             try {
-                BaseClientPacket packet = packetQueue.poll(1, TimeUnit.SECONDS);
+                BaseClientPacket packet = getNextPacket();
                 if (packet != null) {
                 	byte[] bytes = packet.getBytes();
                 	outputStream.write(bytes);
+                } else {
+                	long newTime = System.currentTimeMillis() - queueTime;
+                	if (newTime > 0) {
+                		synchronized(this) {
+                			this.wait(newTime);
+                		}
+                	}
                 }
             } catch (InterruptedException e) {
             	log.error("", e);
@@ -45,8 +58,30 @@ public class ChatPacketSender extends Thread {
         	log.error("", e);
         }
 	}
+	
+	private BaseClientPacket getNextPacket() throws InterruptedException {
+		if (throttledPacketQueue.peek() != null) {
+			if (queueTime < System.currentTimeMillis()) {
+				queueTime = Math.max(System.currentTimeMillis() - TIME_BETWEEN_PACKETS * BURST_PACKETS, queueTime);
+				queueTime += TIME_BETWEEN_PACKETS;
+				return throttledPacketQueue.poll();
+			}
+		}
+		return packetQueue.poll();
+	}
 
 	public void sendPacket(BaseClientPacket packet) {
-		packetQueue.add(packet);
+		switch (packet.getPacketType()) {
+			case ChannelMessage.TYPE:
+			case PrivateMessageSend.TYPE:
+				throttledPacketQueue.add(packet);
+				break;
+			default:
+				packetQueue.add(packet);
+				break;
+		}
+		synchronized(this) {
+			notify();
+		}
 	}
 }
