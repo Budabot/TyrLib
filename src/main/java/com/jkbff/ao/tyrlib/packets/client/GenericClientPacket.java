6aa2f42d9f7a316f5d511f99edf5edfc7f80138e
Tyrence <email1@jkbff.com>
2018-01-14 09:23:03 -0700

    added GenericClientPacket and GenericServerPacket

---
+++ src/main/java/com/jkbff/ao/tyrlib/packets/client/GenericClientPacket.java
@@ -0,0 +1,34 @@
+package com.jkbff.ao.tyrlib.packets.client;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class GenericClientPacket extends BaseClientPacket {
+    protected final int packetType;
+    protected final byte[] data;
+
+    public GenericClientPacket(int packetType, int size, DataInputStream input) {
+        this.packetType = packetType;
+        this.data = new byte[size];
+        try {
+            input.read(this.data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public GenericClientPacket(int packetType, byte[] data) {
+        this.packetType = packetType;
+        this.data = data;
+    }
+
+    @Override
+    public int getPacketType() {
+        return this.packetType;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return data;
+    }
+}
---
+++ src/main/java/com/jkbff/ao/tyrlib/packets/server/GenericServerPacket.java
@@ -0,0 +1,34 @@
+package com.jkbff.ao.tyrlib.packets.server;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class GenericServerPacket extends BaseServerPacket {
+    protected final int packetType;
+    protected final byte[] data;
+
+    public GenericServerPacket(int packetType, int size, DataInputStream input) {
+        this.packetType = packetType;
+        this.data = new byte[size];
+        try {
+            input.read(this.data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public GenericServerPacket(int packetType, byte[] data) {
+        this.packetType = packetType;
+        this.data = data;
+    }
+
+    @Override
+    public int getPacketType() {
+        return this.packetType;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return data;
+    }
+}
